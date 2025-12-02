6777ef4298096cf1cb4baba25ff9ac836a4b5892
Renato <eltrue@outlook.es>
2025-12-01 23:37:43 -0500

    feat: nuevos getters para Id y TypeAccount

--- app/src/main/java/uns/saborea/networking/ModelResponse.java
+++ app/src/main/java/uns/saborea/networking/ModelResponse.java
@@ -1,14 +1,35 @@
 package uns.saborea.networking;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ModelResponse {
+
+    @SerializedName("status")
     private String status;
+
+    @SerializedName("message")
     private String message;
 
+    @SerializedName("user_id")
+    private int userId;
+
+    @SerializedName("tipo_cuenta")
+    private String accountType;
+
+    // Métodos Getters
     public String getStatus() {
         return status;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getAccountType() {
+        return accountType;
     }
 }
